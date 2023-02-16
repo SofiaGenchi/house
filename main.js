01bc00396d8fd277797fea6e6c0e38dd17f09c57
Sofia Genchi <sofigen16@gmail.com>
2023-02-15 22:19:01 -0300

    value accordion

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-function scrollHeader(){
-    const header = document.getElementById('header')
+function scrollHeader() {
+  const header = document.getElementById("header");
 
-    if(this.scrollY >= 50) header.classList.add('scroll-header');
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
+
+const accordionItems = document.querySelectorAll(".value__accordion-item");
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".value__accordion-header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector('.accordion-open')
+    toggleItem(item);
+
+    if(openItem && openItem!== item){
+        toggleItem(openItem)
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value__accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
